93ff6b8ba8e57b4bd78f318614751af55990c111
jjayaraaj <jjayaraaj@gmail.com>
2023-11-16 12:26:01 +0530

    Industry update

--- dist/app.js
+++ dist/app.js
@@ -1,110 +1,116 @@
 $(function () {
-  var footer = `  <footer>
-<div class="py-16 bg-gray-light ">
-    <div class="w-container space-y-10">
-        <div class=" flex flex-col md:flex-row gap-5 md:gap-0 justify-between ">
-            <div>
-                <img src="./../dist/images/kggl-white-logo.png" alt="">
-            </div>
-            <div class=" grid grid-cols-1 md:grid-cols-3 gap-6 md:gap-10">
-                <div class="flex  items-center  gap-x-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" class="w-8 h-8 text-primary-50">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
-                    </svg>
-                    <div>
-                        <div class=""> Call Us</div>
-                        <p class="font-semibold">+91 89736 47299</p>
-                    </div>
+  var footer = `      <footer>
+  <div class="py-16 bg-gray-light ">
+      <div class="w-container space-y-10">
+          <div class=" flex flex-col md:flex-row gap-5 md:gap-0 justify-between ">
+              <div>
+                  <img src="./../dist/images/kggl-white-logo.png" alt="">
+              </div>
+              <div class=" grid grid-cols-1 md:grid-cols-3 gap-6 md:gap-10">
+                  <div class="flex  items-center  gap-x-2">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                          stroke="currentColor" class="w-8 h-8 text-primary-50">
+                          <path stroke-linecap="round" stroke-linejoin="round"
+                              d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
+                      </svg>
+                      <div>
+                          <div class=""> Call Us</div>
+                          <p class="font-semibold">+91 89736 47299</p>
+                      </div>
 
-                </div>
+                  </div>
 
-                <div class="flex items-center  gap-x-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" class="w-8 h-8 text-primary-50">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
-                    </svg>
-                    <div>
-                        <div> Send Us Mail</div>
-                        <p class="font-semibold">info@kggl.com</p>
-                    </div>
+                  <div class="flex items-center  gap-x-2">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                          stroke="currentColor" class="w-8 h-8 text-primary-50">
+                          <path stroke-linecap="round" stroke-linejoin="round"
+                              d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
+                      </svg>
+                      <div>
+                          <div> Send Us Mail</div>
+                          <p class="font-semibold">info@kggl.com</p>
+                      </div>
 
-                </div>
+                  </div>
 
-                <div class="flex items-center  gap-x-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" class="w-8 h-8 text-primary-50">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
-                    </svg>
-                    <div>
-                        <div>Office hours</div>
-                        <p class="font-semibold">10 AM - 6 PM</p>
-                    </div>
+                  <div class="flex items-center  gap-x-2">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                          stroke="currentColor" class="w-8 h-8 text-primary-50">
+                          <path stroke-linecap="round" stroke-linejoin="round"
+                              d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
+                      </svg>
+                      <div>
+                          <div>Office hours</div>
+                          <p class="font-semibold">10 AM - 6 PM</p>
+                      </div>
 
-                </div>
-
-
-
-            </div>
-        </div>
+                  </div>
 
 
-        <div class=" grid grid-cols-1 md:grid-cols-4 gap-x-10">
 
-            <div class=" space-y-3">
-                <div class="heading">Our Moto</div>
-                <p>We are able to guarantee a very high level of satisfaction for our clients. Pharetra libero
-                    non facilisis imperdiet, mi augue feugiat nisl sit amet mollis enim velit Vestibulum
-                    fringilla nulla ultricies.</p>
-            </div>
-
-            <div>
-                <div class="heading">Industries</div>
-                <div class="divide-y divide-dotted  flex flex-col">
-                    <a href="#" class="py-3">Tunneling</a>
-                    <a href="#" class="py-2">Steel</a>
-                    <a href="#" class="py-2">Soap / Detergent and Hygiene</a>
-                    <a href="#" class="py-2">Ceramic</a>
-                    <a href="#" class="py-2">Oil Field and Drilling</a>
-                </div>
-            </div>
+              </div>
+          </div>
 
 
-            <div>
-                <div class="heading text-gray-light">Industries</div>
-                <div class="divide-y divide-dotted  flex flex-col">
-                    <a href="#" class="py-3">Water Treatment</a>
-                    <a href="#" class="py-2">Foundries</a>
-                    <a href="#" class="py-2">Packing and Adhesives</a>
-                    <a href="#" class="py-2">Ceramic</a>
-                    <a href="#" class="py-2">Concretes and General</a>
-                </div>
-            </div>
+          <div class=" grid grid-cols-1 md:grid-cols-4 gap-x-10">
+
+              <div class=" space-y-3">
+                  <div class="heading">Our Moto</div>
+                  <p>We are able to guarantee a very high level of satisfaction for our clients. Pharetra libero
+                      non facilisis imperdiet, mi augue feugiat nisl sit amet mollis enim velit Vestibulum
+                      fringilla nulla ultricies.</p>
+              </div>
+
+              <div>
+                  <div class="heading">Industries</div>
+                  <div class="divide-y divide-dotted  flex flex-col">
+                      <a href="#" class="py-3">Tunneling</a>
+                      <a href="#" class="py-2">Steel</a>
+                      <a href="#" class="py-2">Soap / Detergent and Hygiene</a>
+                      <a href="#" class="py-2">Ceramic</a>
+                      <a href="#" class="py-2">Oil Field and Drilling</a>
+                  </div>
+              </div>
 
 
-            <div class=" flex items-center">
-                <div>
-                    <a class="btn btn-yellow">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                            stroke-width="1.5" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m.75 12l3 3m0 0l3-3m-3 3v-6m-1.5-9H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
-                        </svg>
-
-                        Concretes and General</a>
-                </div>
-
-            </div>
+              <div>
+                  <div class="heading text-gray-light">Industries</div>
+                  <div class="divide-y divide-dotted  flex flex-col">
+                      <a href="#" class="py-3">Water Treatment</a>
+                      <a href="#" class="py-2">Foundries</a>
+                      <a href="#" class="py-2">Packing and Adhesives</a>
+                      <a href="#" class="py-2">Ceramic</a>
+                      <a href="#" class="py-2">Concretes and General</a>
+                  </div>
+              </div>
 
 
-        </div>
+              <div class=" flex items-center">
+                  <div class=" justify-center space-y-3">
+                      <div
+                          class="bg-white mx-auto rounded-full h-32 w-32 p-3 shadow-lg flex items-center justify-center ">
+                          <img src="./../dist/images/kiran-logo.jpg" alt="" class="rounded-full ">
+                      </div>
+                      <div>
+                          <a class="btn btn-yellow">
+                              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                  stroke-width="1.5" stroke="currentColor">
+                                  <path stroke-linecap="round" stroke-linejoin="round"
+                                      d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m.75 12l3 3m0 0l3-3m-3 3v-6m-1.5-9H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
+                              </svg>
 
-    </div>
-</div>
-<div class="h-7 bg-gray-regular"></div>
+                              Concretes and General</a>
+                      </div>
+                  </div>
+
+              </div>
+
+
+          </div>
+
+      </div>
+  </div>
+  <div class="h-7 bg-gray-regular"></div>
 </footer>
 `;
 
@@ -128,8 +134,21 @@ $(function () {
   $("#navDivs").html(nav);
   $("#footerDiv").html(footer);
 
-  $(".owl-carousel").owlCarousel({
+  $(".owl-banner").owlCarousel({
     items: 1,
     dots: true,
+    autoplay: true,
+    autoplayTimeout: 10000,
+    loop: true,
+  });
+
+  //industr 1
+
+  $(".owl-industry1").owlCarousel({
+    items: 1,
+    dots: true,
+    margin: 10,
+    autoplay: true,
+    loop: true,
   });
 });
